3b5487a41e0fe425bf41fecc6b88334b786dda33
Sandesh Athawale <61369792+sandesh1409@users.noreply.github.com>
2020-09-15 11:10:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -64,6 +64,15 @@ def predict():
 #    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
 #
 #@app.route('/predict_api',methods=['POST',"GET"])
+#def predict_api():
+#    '''
+#    For direct API calls trought request
+#    '''
+#    data = request.get_json(force=True)
+#    prediction = model.predict([np.array(list(data.values()))])
+
+#    output = prediction[0]
+#    return jsonify(output)
 
 
 
